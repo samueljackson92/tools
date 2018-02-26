b948e250f66b3bdfb13c0a903277161020092488
Samuel Jackson <samueljackson@outlook.com>
2018-02-26 16:43:24 +0000

    Checkpoint work on tools

--- castep_remote.py
+++ castep_remote.py
@@ -1,24 +1,23 @@
-import os
-import paramiko
+#!/usr/bin/env python
 import argparse as ap
+from scp import SCPClient
 from soprano.hpc.submitter.queues import QueueInterface
 
 if __name__ == "__main__":
     parser = ap.ArgumentParser(description='Run a batch of structures with DFTB+',
                                formatter_class=ap.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('location', type=str,
-                        help='Username and host address to connect to')
+    parser.add_argument('host', type=str,
+                        help='Host address to connect to')
+    parser.add_argument('directory', type=str,
+                        help='Directory of structures to submit to server')
+
     args = parser.parse_args()
-    user, host = args.location.split("@")
 
     queue = QueueInterface.LSF()
-    queue.set_remote_host(host)
-    print (queue.list())
+    queue.set_remote_host(args.host)
 
-    # c = paramiko.SSHClient()
-    # c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    # c.connect(hostname=host, username=user, look_for_keys=True)
-    # print("Connected to {}@{}".format(user, host))
-    # stdin, stdout, stderr = c.exec_command("bjobs")
-    # print (stdout.read())
-    # c.close()
+    with queue._rTarg.context as remote:
+        scp = SCPClient(remote._client.get_transport())
+        scp.put(args.directory, recursive=True,
+                remote_path='~/{}'.format(args.directory))
+        scp.close()
--- dftbconv.py
+++ dftbconv.py
@@ -2,15 +2,16 @@
 import os
 import numpy as np
 import argparse as ap
+import glob
 
 import yaml
 import subprocess
 from schema import Schema, Optional, SchemaError
 
 from ase import io
+from ase.calculators.singlepoint import SinglePointCalculator
 from _muairss.__main__ import save_dftb_format, safe_create_folder
 from _muairss.input import load_input_file
-from _muairss.dftb import load_dtfb
 
 import matplotlib.pyplot as plt
 
@@ -19,6 +20,135 @@ conv_schema = Schema({
     Optional('kpoint_n_min', default=1): int,
     Optional('kpoint_n_max', default=4): int,
 })
+
+
+def tag_muon(atoms):
+    """ Set the tag for a muon in an atoms object
+
+    This currently assumes that the muon is the *last* atom
+    in the Atoms object.
+
+    | Args:
+    |   atoms (ase.Atoms): an Atoms object to set the tag for
+    |
+    | Returns:
+    |   tags (np.array): an updated array of tags where the muon
+    |       is set to 1 and all other atoms are set to 0.
+    """
+    # this assumes that the LAST element in the file is the muon!
+    tags = atoms.get_tags()
+    tags[-1] = 1
+    return tags
+
+
+def load_cell(file_name):
+    atoms_orig = io.read(file_name.replace("geo_end.xyz", "geo_end.gen"))
+    return atoms_orig.cell
+
+
+def load_dtfb(file_name):
+    # Load the final positions from the .xyz file
+    atoms = io.read(file_name)
+    # Load the unit cell from the input .gen file
+    atoms.cell = load_cell(file_name)
+    atoms.set_tags(tag_muon(atoms))
+
+    try:
+        spcalc = DftbSinglePointCalculator(os.path.dirname(file_name), atoms)
+        atoms.set_calculator(spcalc)
+    except IOError:
+        # if there was no calculator then set it to None
+        atoms.set_calculator(None)
+
+    return atoms
+
+
+class DftbSinglePointCalculator(SinglePointCalculator):
+    """Single point calculator for DFTB+
+
+    This loads the results Dftb+. This differs from the standard Dftb
+    calculator in that it will not remove the output file on load.
+   """
+
+    def __init__(self, directory, atoms):
+        self.directory = directory
+        self.atoms = atoms
+        energy, forces, charges = self.read_results(directory)
+        results = {}
+        results['energy'] = energy
+        results['charges'] = charges
+        results['forces'] = forces
+        SinglePointCalculator.__init__(self, atoms, **results)
+
+    def read_results(self, directory):
+        results_file = os.path.join(self.directory, 'results.tag')
+        with open(results_file, 'r') as f_handle:
+            lines = f_handle.readlines()
+
+        charges = self.read_charges(directory)
+        energy = self.read_energy(lines)
+        forces = self.read_forces(lines)
+
+        return energy, forces, charges
+
+    def read_energy(self, lines):
+        """Read Energy from dftb output file (results.tag)."""
+        from ase.units import Hartree
+        # Energy line index
+        for iline, line in enumerate(lines):
+            estring = 'total_energy'
+            if line.find(estring) >= 0:
+                index_energy = iline + 1
+                break
+        try:
+            return float(lines[index_energy].split()[0]) * Hartree
+        except:
+            raise RuntimeError('Problem in reading energy')
+
+    def read_forces(self, lines):
+        """Read Forces from dftb output file (results.tag)."""
+        from ase.units import Hartree, Bohr
+
+        # Force line indexes
+        for iline, line in enumerate(lines):
+            fstring = 'forces   '
+            if line.find(fstring) >= 0:
+                index_force_begin = iline + 1
+                line1 = line.replace(':', ',')
+                index_force_end = iline + 1 + \
+                    int(line1.split(',')[-1])
+                break
+        try:
+            gradients = []
+            for j in range(index_force_begin, index_force_end):
+                word = lines[j].split()
+                gradients.append([float(word[k]) for k in range(0, 3)])
+            return np.array(gradients) * Hartree / Bohr
+        except:
+            raise RuntimeError('Problem in reading forces')
+
+    def read_charges(self, directory):
+        """ Get partial charges on atoms
+            in case we cannot find charges they are set to None
+        """
+        detailed_file = os.path.join(self.directory, 'detailed.out')
+        with open(detailed_file, 'r') as f_handle:
+            lines = f_handle.readlines()
+
+        qm_charges = []
+        for n, line in enumerate(lines):
+            if ('Atom' and 'Net charge' in line):
+                chargestart = n + 1
+                break
+        else:
+            # print('Warning: did not find DFTB-charges')
+            # print('This is ok if flag SCC=NO')
+            return None
+        lines1 = lines[chargestart:(chargestart + len(self.atoms))]
+        for line in lines1:
+            qm_charges.append(float(line.split()[-1]))
+
+        return np.array(qm_charges)
 
 
 def makefig():
@@ -48,16 +178,15 @@ def write_mutiple_parameter(args, conv_params):
         raise RuntimeError("Input seed must be a folder")
 
     params = load_input_file(args.parameter_file)
-
     kpt_range = range(conv_params['kpoint_n_min'], conv_params['kpoint_n_max']+1)
 
     folder_names = []
     for num_kpts in kpt_range:
         params['k_points_grid'] = np.array([num_kpts]*3)
 
-        name = os.path.basename(args.seedname) + "_k{}".format(num_kpts)
-        folder_name = os.path.join(args.seedname, name)
-        folder_name = safe_create_folder(folder_name)
+        ext = "_k{}".format(num_kpts)
+        name = os.path.basename(args.seedname) + ext
+        folder_name = safe_create_folder(name)
 
         save_dftb_format(struct, folder_name, params)
         folder_names.append(folder_name)
@@ -104,15 +233,29 @@ if __name__ == "__main__":
     parser.add_argument('seedname', type=str, default=None,
                         help="A structure file or a folder of files in an ASE "
                         "readable format")
-    parser.add_argument('parameter_file', type=str, default=None, help="""YAML
+    parser.add_argument('-p', '--parameter_file', type=str, default=None, required=False, help="""YAML
                         formatted file with parameters.""")
-    parser.add_argument('conv_file', type=str, default=None, help="""YAML
+    parser.add_argument('-c', '--conv_file', type=str, default=None, required=False, help="""YAML
                         formatted file with parameters to vary.""")
+    parser.add_argument('-m', '--mode', choices=['input', 'output', 'all'],
+                        help="Only generate input folders")
     args = parser.parse_args()
 
-    conv_params = load_conv_file(args.conv_file)
-    folder_names = write_mutiple_parameter(args, conv_params)
+    if args.parameter_file is None:
+        args.parameter_file = args.seedname + ".yaml"
 
-    map(run_dftb, folder_names)
-    generate_results(folder_names)
+    if args.conv_file is None:
+        args.conv_file = args.seedname + ".conv"
+
+    conv_params = load_conv_file(args.conv_file)
+    if args.mode == "input" or args.mode == "all":
+        folder_names = write_mutiple_parameter(args, conv_params)
+ 
+    if args.mode == "all":
+        map(run_dftb, folder_names)
+
+    if args.mode == "output" or args.mode == "all":
+        pattern = os.path.basename(args.seedname) + "_k*"
+        folder_names = glob.glob(pattern)
+        generate_results(folder_names)
 
